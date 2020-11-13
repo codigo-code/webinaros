887fbc74f03affcd56f16ba9562275dcf2ffe0b2
neolabar <dante.panella.contractor@gdevtools.com>
2020-11-13 11:26:01 -0300

    Address

--- src/main/java/com/globalcampus/controlador/HolaController.java
+++ src/main/java/com/globalcampus/controlador/HolaController.java
@@ -1,6 +1,7 @@
 package com.globalcampus.controlador;
 
-import org.springframework.http.ResponseEntity;
+import java.net.InetAddress;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,14 @@ public class HolaController {
 
 	@RequestMapping(value="saludar")
 	public String holaMundo(Model model) {
-		model.addAttribute("objHola", "Hola mundo!");
+		try {
+			model.addAttribute("objHola", "Hola mundo!");
+			model.addAttribute("servidor", InetAddress.getLocalHost().getHostName());
+			model.addAttribute("ip", InetAddress.getLoopbackAddress());
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		return "saludar";
 	}
 }
